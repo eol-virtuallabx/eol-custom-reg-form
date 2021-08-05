083d7b5673b945f6fd59c31222252525770fdfc0
konialtamirano <cons.altamirano@gmail.com>
2021-08-05 15:39:10 -0400

    Update setup.py and apps.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="eol-custom-reg-form",
+    version="0.0.1",
+    author="constanzaaltamirano",
+    author_email="constanzaaltamirano@uchile.cl",
+    description="Eol custom reg form",
+    long_description="Eol custom reg form",
+    url="https://cajalosandes.virtual-labx.cl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "lms.djangoapp": [
+            "eol-custom-reg-form = custom_reg_form.apps:EolCustomRegFormConfig",
+        ]
+    },
+)
