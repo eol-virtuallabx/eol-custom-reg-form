bcc9233281392e916c789a6e244c933b928bf42b
Luis Santana <luis.santana101@gmail.com>
2021-08-31 16:07:24 -0400

    add admin fields

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import ExtraInfo
 
-admin.site.register(ExtraInfo)
+class ExtraInfoAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+    list_display = ('labx_rut', 'user')
+    search_fields = ['labx_rut', 'user__username']
+    ordering = ['-labx_rut']
+
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
